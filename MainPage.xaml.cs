a3954dccd43cd87ca614cb89ae428105c740e00a
Yonatan <yonatanyonyonatan@gmail.com>
2025-07-21 18:56:55 -0500

    Agregar archivos de proyecto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using NCalc;
+namespace Calculadora
+
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var boton = sender as Button;
+            if (boton == null) return;
+            if (boton.Text == "0" && string.IsNullOrEmpty(lblOperacion.Text)) return;
+            if (boton.Text == "+" || boton.Text == "-" || boton.Text == "/" || boton.Text == "X")
+            {
+                if (lblOperacion.Text == null || lblOperacion.Text == "") return;
+                char ultimo = lblOperacion.Text[lblOperacion.Text.Length - 1];
+                if (ultimo == '/' || ultimo == '-' || ultimo == '+' || ultimo == '*')
+                {
+                    return;
+                }
+                else
+                {
+                    if (boton.Text =="X")
+                    {
+                        lblOperacion.Text += "*";
+                    }
+                    else
+                    {
+                        lblOperacion.Text += boton.Text;    
+                    }
+                    return;
+                    
+                }
+            }
+            int numero = 0;
+            bool r = int.TryParse(boton.Text, out numero);
+            if (r)
+            {
+                lblOperacion.Text += numero.ToString();
+                return;
+            }
+            else if (boton.Text == "C")
+            {
+                lblOperacion.Text = lblResultado.Text = string.Empty;
+                return;
+            }else if (boton.Text == "=" && !string.IsNullOrEmpty(lblOperacion.Text))
+            {
+                string operacion = lblOperacion.Text;
+                var exp = new Expression(operacion);
+                lblResultado.Text = exp.Evaluate().ToString();
+            }
+        }
+    }
+}
